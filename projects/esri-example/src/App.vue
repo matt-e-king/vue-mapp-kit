72e370550aa8b2ae7ede50f886fce68fe32d8dbf
Matthew King <mking@crownchimp.com>
2020-05-27 13:48:50 -0700

    chore: minor change to example App.vue for esri

--- projects/esri-example/src/App.vue
+++ projects/esri-example/src/App.vue
@@ -296,13 +296,9 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   height: 100%;
-  position: relative;
+  display: flex;
+  flex-direction: column;
 }
 
 button {
