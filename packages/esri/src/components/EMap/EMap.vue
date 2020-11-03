8481fb4327c975113d7646d687a8edfbd08b873b
Matthew King <mking@crownchimp.com>
2020-11-03 14:57:48 -0700

    chore: code formatting; publish command in root

--- packages/esri/src/components/EMap/EMap.vue
+++ packages/esri/src/components/EMap/EMap.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="map">
-    <slot v-if="booted" v-bind:map="module.Map"/>
+    <slot
+      v-if="booted"
+      v-bind:map="module.Map"
+    />
   </div>
 </template>
 
@@ -61,5 +64,3 @@ export default {
   }
 }
 </script>
-
-<style></style>
